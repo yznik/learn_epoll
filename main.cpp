0c46a77e75eaf2016fed2366e7750018f1f2a4e5
yznik <yznik93@gmail.com>
2020-02-28 02:41:08 +0300

    adding bind

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <optional>
+#include <arpa/inet.h>
 
 class Socket
 {
@@ -18,7 +19,7 @@ public:
     static std::optional<Socket> createIPv4TCPSocket()
     {
         //@todo: see SOCK_NONBLOCK into type parameter;
-        auto fd = socket(AF_INET, SOCK_STREAM, 0);
+        auto fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
         if (fd == -1)
         {
             std::cout << -1 << std::endl;
@@ -37,6 +38,55 @@ public:
         type = s.type;
         s.fd = -1;
     }
+
+    bool bindIPv4(std::string address, uint16_t port)
+    {
+        struct sockaddr_in sa = {
+            domain,
+            htons(port),
+            0,
+            0
+        };
+
+        switch (inet_pton(sa.sin_family, address.c_str(), &sa.sin_addr))
+        {
+        case 1:
+            break;
+        case 0:
+            std::cerr << "cannot convert to ipv4 format" << std::endl;
+            return false;
+        default:
+            std::cerr << strerror(errno) << std::endl;
+            return false;
+        }
+
+        if (bind(fd, reinterpret_cast<sockaddr*>(&sa), sizeof(sa)) < 0)
+        {
+            std::cerr << strerror(errno) << std::endl;
+            return false;
+        }
+
+        return true;
+    }
+
+    bool bindIPv4(uint32_t address, uint16_t port)
+    {
+        struct sockaddr_in sa = {
+            domain,
+            htons(port),
+            htonl(address),
+            0
+        };
+
+        if (bind(fd, reinterpret_cast<sockaddr*>(&sa), sizeof(sa)) < 0)
+        {
+            std::cerr << strerror(errno) << std::endl;
+            return false;
+        }
+
+        return true;
+    }
+
     ~Socket()
     {
         if (fd != -1)
@@ -56,7 +106,7 @@ public:
 private:
     int fd = -1;
     int type;
-    int domain;
+    sa_family_t domain;
     bool connected = false;
 };
 
